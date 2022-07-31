0dce1ce190c70b670f633250797806cfead9bd9d
Leonardo Otero <leonardo.otero390@gmail.com>
2022-07-30 22:01:50 -0400

    feat: isHappy function

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,22 @@
+function numbersSum(number: Number): Number {
+  const numbersArray = number.toString().split('');
+  return numbersArray.reduce((acc, curr) => acc + Number(curr) ** 2, 0);
+}
+
+function isHappy(number: Number): Boolean {
+  let candidate = number;
+  for (let i = 0; i < 100; i++) {
+    const sum = numbersSum(candidate);
+    if (sum === 1) {
+      return true;
+    }
+    candidate = sum;
+  }
+  return false;
+}
+
 export default function app(number: Number): String {
-  return 'Hello World';
+  const happy = isHappy(number) ? 'Feliz' : 'Não-Feliz';
+
+  return `Número Sortudo e ${happy}.`;
 }
