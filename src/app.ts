cf829d4d39f27f1a7d2dad664ca055e06770df52
Leonardo Otero <leonardo.otero390@gmail.com>
2022-07-30 22:50:45 -0400

    refac: object-oriented solution

--- src/app.ts
+++ src/app.ts
@@ -1,45 +1,62 @@
-function numbersSum(number: Number): Number {
-  const numbersArray = number.toString().split('');
-  return numbersArray.reduce((acc, curr) => acc + Number(curr) ** 2, 0);
-}
+class LuckyHappyNumber {
+  number: Number;
 
-function isHappy(number: Number): Boolean {
-  let candidate = number;
-  for (let i = 0; i < 100; i++) {
-    const sum = numbersSum(candidate);
-    if (sum === 1) {
-      return true;
+  constructor(number: Number) {
+    this.number = number;
+  }
+
+  public isHappy(): Boolean {
+    function numbersSum(num: Number): Number {
+      const numbersArray = num.toString().split('');
+      return numbersArray.reduce((acc, curr) => acc + Number(curr) ** 2, 0);
     }
-    candidate = sum;
-  }
-  return false;
-}
 
-function buildArray(number: Number): Number[] {
-  const result = [];
-  for (let index = 1; index <= number; index++) {
-    result.push(index);
+    let candidate = this.number;
+    for (let i = 0; i < 100; i++) {
+      const sum = numbersSum(candidate);
+      if (sum === 1) {
+        return true;
+      }
+      candidate = sum;
+    }
+    return false;
   }
-  return result;
-}
 
-function isLucky(number: Number): Boolean {
-  let array = buildArray(number);
-  array = array.filter((item) => Number(item) % 2 !== 0);
-  if (!array.includes(number)) return false;
-  let position = 1;
-  while (array.length > 1 && position < array.length) {
-    const multiplyer = Number(array[position]);
-    array = array.filter((item, index) => (index + 1) % multiplyer !== 0);
-    if (!array.includes(number)) return false;
-    position++;
+  public isLucky(): Boolean {
+    function buildArray(number: Number): Number[] {
+      const result = [];
+      for (let index = 1; index <= number; index++) {
+        result.push(index);
+      }
+      return result;
+    }
+
+    let array = buildArray(this.number);
+    array = array.filter((item) => Number(item) % 2 !== 0);
+    if (!array.includes(this.number)) return false;
+
+    let position = 1;
+    while (array.length > 1 && position < array.length) {
+      const multiplyer = Number(array[position]);
+      array = array.filter((item, index) => (index + 1) % multiplyer !== 0);
+      if (!array.includes(this.number)) return false;
+      position++;
+    }
+    return true;
   }
-  return true;
+
+  public getMessage(): String {
+    const happy = this.isHappy() ? 'Feliz' : 'Não-Feliz';
+    const lucky = this.isLucky() ? 'Sortudo' : 'Não-Sortudo';
+
+    return `Número ${lucky} e ${happy}.`;
+  }
 }
 
 export default function app(number: Number): String {
-  const happy = isHappy(number) ? 'Feliz' : 'Não-Feliz';
-  const lucky = isLucky(number) ? 'Sortudo' : 'Não-Sortudo';
+  const result = new LuckyHappyNumber(number);
+  const happy = result.isHappy() ? 'Feliz' : 'Não-Feliz';
+  const lucky = result.isLucky() ? 'Sortudo' : 'Não-Sortudo';
 
   return `Número ${lucky} e ${happy}.`;
 }
