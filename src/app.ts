58380cdf7700d479e62eb34ee473b863e5b8b400
Leonardo Otero <leonardo.otero390@gmail.com>
2022-07-30 22:40:07 -0400

    feat: isLucky Function

--- src/app.ts
+++ src/app.ts
@@ -15,8 +15,31 @@ function isHappy(number: Number): Boolean {
   return false;
 }
 
+function buildArray(number: Number): Number[] {
+  const result = [];
+  for (let index = 1; index <= number; index++) {
+    result.push(index);
+  }
+  return result;
+}
+
+function isLucky(number: Number): Boolean {
+  let array = buildArray(number);
+  array = array.filter((item) => Number(item) % 2 !== 0);
+  if (!array.includes(number)) return false;
+  let position = 1;
+  while (array.length > 1 && position < array.length) {
+    const multiplyer = Number(array[position]);
+    array = array.filter((item, index) => (index + 1) % multiplyer !== 0);
+    if (!array.includes(number)) return false;
+    position++;
+  }
+  return true;
+}
+
 export default function app(number: Number): String {
   const happy = isHappy(number) ? 'Feliz' : 'Não-Feliz';
+  const lucky = isLucky(number) ? 'Sortudo' : 'Não-Sortudo';
 
-  return `Número Sortudo e ${happy}.`;
+  return `Número ${lucky} e ${happy}.`;
 }
